aef39e595e633e9aa6c4c024ab06107ede1a9d99
fixees <112260686+fixees@users.noreply.github.com>
2022-09-26 18:43:48 +0500

    обновлено v0.0.4

--- settings.py
+++ settings.py
@@ -8,9 +8,12 @@ prefix_cmd = '.'
 # Ваш айди:
 owner_id = 319990365
 
+
+
+# !!! НЕ АКТУАЛЬНО !!!
 # RCON:
 # Пример: rcon_pass = 'supersecretpassword'
 #     \   rcon_port = 25575
 #      \  rcon_address = '0.0.0.0.0'
-rcon_pass = 'supersecret'
-rcon_address = '0.0.0.0'
+# rcon_pass = 'supersecret'
+# rcon_address = '91.134.234.179'
