a17d57116c59fc789c8aa9c2b15ee90dc84ff373
fixees <112260686+fixees@users.noreply.github.com>
2022-09-26 18:42:09 +0500

    обновлено v0.0.4

--- _permission_rule.py
+++ _permission_rule.py
@@ -1,9 +1,19 @@
 from vkbottle.dispatch.rules import ABCRule
 from vkbottle.user import Message
 
+import json
+from settings import owner_id
+
 from settings import owner_id
 
 
 class Permission(ABCRule[Message]):
     async def check(self, event: Message) -> bool:
-        return event.from_id == owner_id
+        with open("servers.json", "r+", encoding='utf-8') as jsonFile:
+            data = json.load(jsonFile)
+        for a in data["users"]:
+            if str(event.from_id) in a['id']:
+                return True
+
+        if event.from_id == owner_id:
+            return True
